f19b7383d9d190c7abd61a85e44530f2b32fe0ce
jacketyan <jacketyan@futunn.com>
2020-06-11 21:14:49 +0800

    提交脚本

---
+++ pandasTest1.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import datetime
+
+web_stats = {'Day':[1,2,3,4,5,6],
+             'Visitors':[43,34,65,56,29,76],
+             'Bounce Rate':[65,67,78,65,45,52]}
+
+df = pd.DataFrame(web_stats)
+df.set_index('Day', inplace=True)
+
+print(df)
